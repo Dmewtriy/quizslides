f869ccacd167b943f89811216ac915f18b370739
santoro <gks130906@gmail.com>
2026-04-02 16:54:53 +0700

    Подключена PostgreSQL, настроены модели и миграции

--- quizslides/settings.py
+++ quizslides/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'quizslides.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quizslides_bd',        
+        'USER': 'postgres',
+        'PASSWORD': 'GroupconnectedBD',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
